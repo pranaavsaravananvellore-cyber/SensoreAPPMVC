8cbcf801426952e791f408ee4818055442de5d55
Pranav <pranaavsaravananvellore@gmail.com>
2025-12-06 12:06:09 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
 using SensoreAPPMVC.Data;
-using SensoreAPPMVC.Models;
 using SensoreAPPMVC.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
@@ -15,26 +13,27 @@ builder.Services.AddSession(options =>
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
-//Add the AppDBContext and configure the connection string
-builder.Services.AddDbContext<AppDBContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<AdminServices>();
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+
+
 var app = builder.Build();
 
-using(var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDBContext>();
-    //apply any pending migrations
+    var dbContext = scope.ServiceProvider.GetRequiredService<SensoreAPPMVC.Data.AppDBContext>();
     dbContext.Database.Migrate();
-    await DbSeeding.dBInitalisation(dbContext);
+    await DbSeeding.DbInitialization(dbContext);
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseHttpsRedirection();
@@ -42,15 +41,13 @@ app.UseStaticFiles();
 app.UseSession();
 app.UseRouting();
 
-app.UseAuthorization();
-
-//app.MapStaticAssets();
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action}/{id?}",
     defaults: new { controller = "Account", action = "Login" });
-    //.WithStaticAssets();
-
 
 app.Run();
+
+
